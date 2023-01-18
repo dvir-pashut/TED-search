691680039420860292690e7344e278cafbaa7f97
dvir.p1999 <dvir.p1999@gmail.com>
2023-01-18 21:08:30 +0000

    still not working on the machine

--- e2e-tests.sh
+++ e2e-tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+check=1
+while [ ${check} == 1 ]
+do
+check=0
+curl -I http://$(cat ec2_ip.txt)/api/search?q=public  || { check=1 ; }
+sleep 1
+done
+
 curl -I http://$(cat ec2_ip.txt)/api/search?q=public | head -n 1 | cut -d " " -f 2 > result.txt
 wait
 curl -I http://$(cat ec2_ip.txt)/api/search?q=city   | head -n 1 | cut -d " " -f 2 >> result.txt
